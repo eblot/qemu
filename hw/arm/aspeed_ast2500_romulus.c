1097739d6fb01dda73865570a741b8f853f05e57
Emmanuel Blot <emmanuel.blot@free.fr>
2026-07-20 11:36:17 +0200

    hw: ds1338: use the TYPE_DS1338 constant instead of the "ds1338" string
    Now that TYPE_DS1338 is exposed by include/hw/rtc/ds1338.h, use it in
    the machines that instantiate the RTC by its raw type name string.
    
    No functional change.
    
    Signed-off-by: Emmanuel Blot <emmanuel.blot@free.fr>

--- hw/arm/aspeed_ast2500_romulus.c
+++ hw/arm/aspeed_ast2500_romulus.c
@@ -11,6 +11,7 @@
 #include "hw/arm/machines-qom.h"
 #include "hw/arm/aspeed.h"
 #include "hw/arm/aspeed_soc.h"
+#include "hw/rtc/ds1338.h"
 
 /* Romulus hardware value: 0xF10AD206 */
 #define ROMULUS_BMC_HW_STRAP1 (                                         \
@@ -30,7 +31,8 @@ static void romulus_bmc_i2c_init(AspeedMachineState *bmc)
      * The romulus board expects Epson RX8900 I2C RTC but a ds1338 is
      * good enough
      */
-    i2c_slave_create_simple(aspeed_i2c_get_bus(&soc->i2c, 11), "ds1338", 0x32);
+    i2c_slave_create_simple(aspeed_i2c_get_bus(&soc->i2c, 11), TYPE_DS1338,
+                            0x32);
 }
 
 static void aspeed_machine_romulus_class_init(ObjectClass *oc,
